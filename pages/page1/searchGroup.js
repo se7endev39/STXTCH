ea23a7b0d5a4ff2ae6ad9e1d5950712f984fe6dd
dreamwell <dreamwell506@mailbox.org>
2021-05-07 04:39:51 +0900

    image resize

--- pages/page1/searchGroup.js
+++ pages/page1/searchGroup.js
@@ -31,7 +31,7 @@ function Filter() {
 function SearchItem({src, comment, stories}) {
     return (
         <div className="m-auto pb-4" style={{width: "90%"}}>
-            <img src={src} className="search_item_img" />
+            <img src={src} className="search_item_img"  style={{width: "-webkit-fill-available"}}/>
             <div className="search_item_caption flex justify-between">
                 <div className="search_item_comment">#{comment}</div>
                 <div className="search_item_stories">{stories.toLocaleString("en")} stories</div>
