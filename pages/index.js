0f141a73b228ff10a5362d627e91f076e087f177
dreamwell <dreamwell506@mailbox.org>
2021-05-07 04:05:06 +0900

    fix bugs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useMemo, useEffect, useCallback } from "react";
+import {useRouter} from 'next/router'
 
 function HomePage() {
-  
+  const router = useRouter()
+  useEffect(() => {
+    router.push(`/Mobile`)
+  }, [])
+
   return (
     <div></div>
   );
