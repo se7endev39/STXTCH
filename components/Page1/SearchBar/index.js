82b39048205f0c504b7a6a7d37bbcfbfc134e653
dreamwell <dreamwell506@mailbox.org>
2021-05-09 22:00:55 +0900

    search group x action

--- components/Page1/SearchBar/index.js
+++ components/Page1/SearchBar/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import search_icon from "assets/images/search.svg";
 import cross_icon from "assets/images/cross.svg";
 import styles from './index.module.scss'
@@ -6,18 +6,20 @@ import styles from './index.module.scss'
 function SearchBar({onQueryChange}) {
 
   const [query, setQuery] = useState("")
-
   
   const onChange = (event) => {
     let newQuery = event.target.value
-    onQueryChange(newQuery)
     setQuery(newQuery)
   }
+
+  useEffect(() => {
+    onQueryChange(query)
+  }, [query])
 
   return (
     <div>
       <div className="flex pl-20 pr-2 pb-5">
-        <img src={query?cross_icon:search_icon} className="pl-2"/>
+        <img onClick={() => setQuery("")} src={query?cross_icon:search_icon} className="pl-2"/>
         <input className={styles.searchtext} onChange={onChange} placeholder="SEARCH GROUPS" value={query}/>
       </div>
       <div className="divider" />
