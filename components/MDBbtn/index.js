15c11ade374051eb148600861f8fe9b5434fe784
dreamwell <dreamwell506@mailbox.org>
2021-05-10 18:42:15 +0900

    tag button problem

--- components/MDBbtn/index.js
+++ components/MDBbtn/index.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './index.module.scss'
+import classnames from 'classnames'
 
-const MDBBtn = ({ className, children, dropdown, ...rest}) => {
+const MDBBtn = ({ className, children, dropdown, onSelect, ...rest}) => {
+  const [showDropdown, setShowDropdown] = useState(false)
+  const dropDownClicked = (tag) => {
+    if(onSelect) onSelect(tag)
+    setShowDropdown(false)
+  }
   return (
     <div className={"wrapper " + dropdown?styles.dropdown: ""}>
-      <button className={className + " MDBbtn"} {...rest}>
+      <button className={className + " MDBbtn"}  onClick={() => setShowDropdown(!showDropdown)} {...rest}>
         {
           children
         }
       </button>
       { 
         dropdown &&
-        <div className={styles.dropdown_Content}>
+        <div className={classnames(styles.dropdown_Content, showDropdown?styles.active:"")}>
           {
             ["tag1", "tag2", "tag3"].map( (tag, i) => (
-              <li key={i}>
+              <li key={i} onClick={dropDownClicked}>
                 {tag}
               </li>
             ))
